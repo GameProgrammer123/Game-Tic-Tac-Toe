032d8c43ec23216f13efc074d67c07aeb34c02fe
GameProgrammer123 <85128978+GameProgrammer123@users.noreply.github.com>
2022-09-29 10:24:16 +0200

    Update boardox.js

--- boardox.js
+++ boardox.js
@@ -199,7 +199,7 @@ setTimeout(() => {
 	if(window.innerWidth < window.innerHeight)
 	document.getElementsByClassName('containerform')[0].style.top = document.getElementsByClassName('button')[1].clientHeight + 6 + (window.innerHeight / 2) + 'px'
 	else document.getElementsByClassName('containerform')[0].style.top = document.getElementsByClassName('button')[1].clientHeight + 6 + 'px';
-	consola.log(document.getElementsByClassName('button')[1].clientHeight + 6 + (window.innerHeight / 2)+' and '+(window.innerHeight / 2));
+	console.log(document.getElementsByClassName('button')[1].clientHeight + 6 + (window.innerHeight / 2)+' and '+(window.innerHeight / 2));
 	document.getElementsByClassName('button')[0].style.height = document.getElementsByClassName('button')[1].clientHeight + 6 + 'px';
 }, 1000);
 }
