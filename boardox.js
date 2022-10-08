f0ffc9b0a4ec4ea15120e4edeb6425c37ab0471b
GameProgrammer123 <85128978+GameProgrammer123@users.noreply.github.com>
2022-10-08 08:59:08 +0200

    Update boardox.js

--- boardox.js
+++ boardox.js
@@ -203,12 +203,17 @@ setTimeout(() => {
 	}
 	
 	if(window.innerWidth < window.innerHeight){
-	  document.getElementsByClassName('containerform')[0].style.top = document.getElementsByClassName('button')[1].clientHeight + 16 + (window.innerHeight / 2) + 'px';
+	  document.getElementsByClassName('button')[0].style.top = 0;
+	  document.getElementsByClassName('button')[1].style.top = 0;
 	  document.getElementsByClassName('button')[0].style.fontSize =	'xxx-large';
 	  document.getElementsByClassName('button')[1].style.fontSize =	'xxx-large';
-	} else { document.getElementsByClassName('containerform')[0].style.top = document.getElementsByClassName('button')[1].clientHeight + 16 + 'px';
+	  document.getElementsByClassName('containerform')[0].style.top = document.getElementsByClassName('button')[1].clientHeight + 16 + (window.innerHeight / 2) + 'px';
+	} else { 
+		 document.getElementsByClassName('button')[0].style.top = 0;
+		 document.getElementsByClassName('button')[1].style.top = 0;
 		 document.getElementsByClassName('button')[0].style.fontSize =	'xx-large';
 	  	 document.getElementsByClassName('button')[1].style.fontSize =	'xx-large';
+		 document.getElementsByClassName('containerform')[0].style.top = document.getElementsByClassName('button')[1].clientHeight + 16 + 'px';
 	  }
 	
 	document.getElementsByClassName('button')[0].style.height = document.getElementsByClassName('button')[1].clientHeight + 6 + 'px';
